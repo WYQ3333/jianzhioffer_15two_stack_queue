6919becbda6d5119653e7788ed2541939b902fe8
浅忆梦微凉 <3538922590@qq.com>
2019-01-25 20:18:15 +0800

    访问越界

--- 剑指offer15_用两个栈实现队列/test.cpp
+++ 剑指offer15_用两个栈实现队列/test.cpp
@@ -26,6 +26,24 @@ private:
 };
 
 void TestFunc(){
+
+	Solution q;
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	q.push(4);
+	q.push(5);
+	q.push(6);
+	int a = q.pop();
+	cout << a << "" << endl;
+	a = q.pop();
+	cout << a << "" << endl;
+	a = q.pop();
+	cout << a << "" << endl;
+	a = q.pop();
+	cout << a << "" << endl;
+	a = q.pop();
+	cout << a << "" << endl;
 	
 }
 
