a8e6301331aede91b1f503d3396334761a3c7bff
浅忆梦微凉 <3538922590@qq.com>
2019-01-25 20:12:06 +0800

    栈编译通过

---
+++ 剑指offer15_用两个栈实现队列/test.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+#include<stack>
+
+
+//class Solution
+//{
+//public:
+//	void push(int node) {
+//		stack1.push(node);
+//	}
+//
+//	int pop() {
+//		
+//	}
+//
+//private:
+//	stack<int> stack1;
+//	stack<int> stack2;
+//};
+
+void test(){
+	
+}
+
+void TestFunc(){
+	stack<int> s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.push(4);
+	s.push(5);
+	
+}
+
+int main(){
+	TestFunc();
+	system("pause");
+	return 0;
+}
